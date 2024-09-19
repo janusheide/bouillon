1ec93be427913a70b5418a9da53f4c831630692a
Janus <janusheide@gmail.com>
2024-09-19 10:25:43 +0200

    ruff checks boil.py (#57)
    * ruff checks boil.py

--- boil.py
+++ boil.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python3
-# encoding: utf-8
 #
 # Copyright (c) 2020, Janus Heide.
 # All rights reserved.
 #
 # Distributed under the "BSD 3-Clause License", see LICENSE.txt.
 
-"""
-Command Line Interface (CLI) for project interaction.
+"""Command Line Interface (CLI) for project interaction.
 
 Run various commands, such as; test, build, release on your project. You should
 modify the steps that are relevant for your project, and the cli such that it
@@ -27,7 +25,7 @@ from typing import Callable
 # Modules that are not part of 'standard' Python is only installed if they can
 # be found, this allows us to run the setup step where they are installed
 # without importing the modules we are about to install.
-if util.find_spec('bouillon') is not None:
+if util.find_spec("bouillon") is not None:
     import bouillon
 
 
@@ -36,7 +34,7 @@ logger = logging.getLogger(__name__)
 
 def setup(*, dry_run: bool, **kwargs) -> None:
     """Install dependencies. Since bouillon is also inste."""
-    logger.info('Installing dependencies')
+    logger.info("Installing dependencies")
 
     if dry_run:
         exit(0)
@@ -50,20 +48,20 @@ def lint(
     liccheck: bool = True,
     mypy: bool = True,
     ruff: bool = True,
-    **kwargs
+    **kwargs,
 ) -> None:
     """Run linters."""
     if isort:
-        bouillon.run(['isort', '.'], **kwargs)
+        bouillon.run(["isort", "."], **kwargs)
 
     if liccheck:
         bouillon.run(["liccheck"], **kwargs)
 
     if ruff:
-        bouillon.run(['ruff', 'check'], **kwargs)
+        bouillon.run(["ruff", "check"], **kwargs)
 
     if mypy:
-        bouillon.run(['mypy', 'src'], **kwargs)
+        bouillon.run(["mypy", "src"], **kwargs)
 
 
 def test(
@@ -71,42 +69,42 @@ def test(
     cicd_tests: bool = True,
     test_files: bool = True,
     unit_tests: bool = True,
-    **kwargs
+    **kwargs,
 ) -> None:
     """Run tests."""
     if test_files:
         if not bouillon.check_for_test_files(
-            os.path.join('src', bouillon.git.repository_name()),
-                os.path.join('test', bouillon.git.repository_name())):
+            os.path.join("src", bouillon.git.repository_name()),
+                os.path.join("test", bouillon.git.repository_name())):
             exit(1)
 
     # https://docs.pytest.org/en/latest/
     # https://pytest-cov.readthedocs.io/en/latest/
     if unit_tests:
         bouillon.run([
-            'pytest',
+            "pytest",
             f'{os.path.join("test", bouillon.git.repository_name())}',
-            '--cov=bouillon',
-            '--cov-report',
-            'term-missing',
-            '--cov-fail-under=95',
-            '--durations=5',
-            '-vv'
+            "--cov=bouillon",
+            "--cov-report",
+            "term-missing",
+            "--cov-fail-under=95",
+            "--durations=5",
+            "-vv",
             ],
             **kwargs)
 
     if cicd_tests:
         bouillon.run([
-            'pytest',
+            "pytest",
             f'{os.path.join("test", "test_boil.py")}',
-            '--durations=5',
-            '-vv'],
+            "--durations=5",
+            "-vv"],
             **kwargs)
 
 
 def upgrade(**kwargs) -> None:
     """Upgrade the versions of the used modules."""
-    bouillon.run(["uppd",], **kwargs)
+    bouillon.run(["uppd"], **kwargs)
     setup(**kwargs)
 
 
@@ -124,56 +122,56 @@ def train(**kwargs) -> None:
 def clean(**kwargs) -> None:
     """Remove files and dirs created during build."""
     logger.info('Deleting "build" and "dist" directories.')
-    shutil.rmtree('dist', ignore_errors=True)
+    shutil.rmtree("dist", ignore_errors=True)
 
 
 def release(*, version: str, **kwargs) -> None:
     """Release the project."""
-    if bouillon.run(['pysemver', 'check', version]).returncode:
+    if bouillon.run(["pysemver", "check", version]).returncode:
         logger.error("Provided version is not valid semver")
         exit(1)
 
-    if not kwargs['dry_run']:
-        if bouillon.git.current_branch() != 'master':
-            logger.error('Only release from the master branch')
+    if not kwargs["dry_run"]:
+        if bouillon.git.current_branch() != "master":
+            logger.error("Only release from the master branch")
             exit(1)
 
         if not bouillon.git.working_directory_clean():
-            logger.error('Unstaged changes in the working directory.')
+            logger.error("Unstaged changes in the working directory.")
             exit(1)
 
         if version in bouillon.git.tags():
             logger.error("Tag already exists.")
             exit(1)
     else:
-        logger.debug('Skipped git status checks.')
+        logger.debug("Skipped git status checks.")
 
     clean(**kwargs)
     lint(**kwargs)
     test(**kwargs)
 
-    logger.debug('Edit the news file using default editor or nano.')
-    EDITOR = os.environ.get('EDITOR', 'nano')
-    bouillon.run([EDITOR, 'NEWS.rst'], **kwargs)
-    bouillon.run(['git', 'add', 'NEWS.rst'], **kwargs)
-    bouillon.run(['git', 'commit', '-m', '"preparing release"'], **kwargs)
+    logger.debug("Edit the news file using default editor or nano.")
+    EDITOR = os.environ.get("EDITOR", "nano")
+    bouillon.run([EDITOR, "NEWS.rst"], **kwargs)
+    bouillon.run(["git", "add", "NEWS.rst"], **kwargs)
+    bouillon.run(["git", "commit", "-m", f"preparing release {version}"], **kwargs)
 
     logger.debug("Create an annotated tag, used by setuptools_scm.")
-    bouillon.run(['git', 'tag', '-a', f'{version}', '-m',
-                  f'creating tag {version} for new release'], **kwargs)
+    bouillon.run(["git", "tag", "-a", f"{version}", "-m",
+                  f"creating tag {version} for new release"], **kwargs)
 
     build(**kwargs)
 
-    logger.debug('upload builds to pypi and push commit and tag to repo.')
-    bouillon.run(['twine', 'upload', 'dist/*'], **kwargs)
-    bouillon.run(['git', 'push'], **kwargs)
-    bouillon.run(['git', 'push', 'origin', f'{version}'], **kwargs)
+    logger.debug("upload builds to pypi and push commit and tag to repo.")
+    bouillon.run(["twine", "upload", "dist/*"], **kwargs)
+    bouillon.run(["git", "push"], **kwargs)
+    bouillon.run(["git", "push", "origin", f"{version}"], **kwargs)
 
 
 def cli() -> Namespace:
     """Build the cli."""
     parser = ArgumentParser(
-        description='Bouillon',
+        description="Bouillon",
         formatter_class=ArgumentDefaultsHelpFormatter,
     )
 
@@ -183,66 +181,66 @@ def cli() -> Namespace:
     parser.set_defaults(check=True, function=_print_help)
 
     parser.add_argument(
-        '--dry-run', action='store_true', help='Perform a dry run.')
+        "--dry-run", action="store_true", help="Perform a dry run.")
     parser.add_argument(
-        '--log-level',
-        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICIAL'],
-        default='WARNING', help='Set log level.')
+        "--log-level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICIAL"],
+        default="WARNING", help="Set log level.")
     parser.add_argument(
-        '--log-file', type=str, help='Set log file.')
+        "--log-file", type=str, help="Set log file.")
 
-    subparsers = parser.add_subparsers(help='Available sub commands')
+    subparsers = parser.add_subparsers(help="Available sub commands")
 
     parser_setup = subparsers.add_parser(
-        'setup',
-        help='Setup installing dependencies, this will execute pip commands.')
+        "setup",
+        help="Setup installing dependencies, this will execute pip commands.")
     parser_setup.set_defaults(function=setup)
 
-    parser_build = subparsers.add_parser('build', help='Build.')
+    parser_build = subparsers.add_parser("build", help="Build.")
     parser_build.set_defaults(function=build)
 
-    parser_lint = subparsers.add_parser('lint', help='Run linters')
+    parser_lint = subparsers.add_parser("lint", help="Run linters")
     parser_lint.set_defaults(function=lint)
     parser_lint.add_argument(
-        '--no-isort', dest='isort', action='store_false',
-        help='Do not run isort.')
+        "--no-isort", dest="isort", action="store_false",
+        help="Do not run isort.")
     parser_lint.add_argument(
-        '--no-liccheck', dest='liccheck', action='store_false',
-        help='Do not check that licenses of all used modules. (Default disabled)',
-        default=False,)
+        "--no-liccheck", dest="liccheck", action="store_false",
+        help="Do not check that licenses of all used modules. (Default disabled)",
+        default=False)
     parser_lint.add_argument(
-        '--no-ruff', dest='ruff', action='store_false',
-        help='Do not check with ruff.')
+        "--no-ruff", dest="ruff", action="store_false",
+        help="Do not check with ruff.")
     parser_lint.add_argument(
-        '--no-mypy-check', dest='mypy', action='store_false',
-        help='Do not perform mypy code analysis.')
+        "--no-mypy-check", dest="mypy", action="store_false",
+        help="Do not perform mypy code analysis.")
 
-    parser_test = subparsers.add_parser('test', help='Run tests')
+    parser_test = subparsers.add_parser("test", help="Run tests")
     parser_test.set_defaults(function=test)
 
     parser_test.add_argument(
-        '--no-test-files-check', dest='test_files', action='store_false',
-        help='Do not check that for each source file there is a test file.')
+        "--no-test-files-check", dest="test_files", action="store_false",
+        help="Do not check that for each source file there is a test file.")
     parser_test.add_argument(
-        '--no-unit-tests', dest='unit_tests', action='store_false',
-        help='Do not run unit tests.')
+        "--no-unit-tests", dest="unit_tests", action="store_false",
+        help="Do not run unit tests.")
     parser_test.add_argument(
-        '--no-cicd-tests', dest='cicd_tests', action='store_false',
-        help='Do not run CICD tests.')
+        "--no-cicd-tests", dest="cicd_tests", action="store_false",
+        help="Do not run CICD tests.")
 
-    parser_train = subparsers.add_parser('train', help='Train.')
+    parser_train = subparsers.add_parser("train", help="Train.")
     parser_train.set_defaults(function=train)
 
     parser_upgrade = subparsers.add_parser(
-        'upgrade', help='upgrade all dependencies.')
+        "upgrade", help="upgrade all dependencies.")
     parser_upgrade.set_defaults(function=upgrade)
 
-    parser_clean = subparsers.add_parser('clean', help='Clean temp files.')
+    parser_clean = subparsers.add_parser("clean", help="Clean temp files.")
     parser_clean.set_defaults(function=clean)
 
-    parser_release = subparsers.add_parser('release', help='release me.')
-    parser_release.add_argument('version', type=str,
-                                help='release version.')
+    parser_release = subparsers.add_parser("release", help="release me.")
+    parser_release.add_argument("version", type=str,
+                                help="release version.")
     parser_release.set_defaults(function=release)
 
     return parser.parse_args()
@@ -251,7 +249,7 @@ def cli() -> Namespace:
 def run(*, function: Callable, log_level: str, log_file: str, **kwargs) -> None:
     """Setup logging and run a step."""
     logging.basicConfig(filename=log_file, level=log_level)
-    if function != setup and util.find_spec('bouillon') is None:
+    if function != setup and util.find_spec("bouillon") is None:
         logger.error('Failed to import bouillon, run "boil setup" first.')
         exit(1)
 
@@ -259,6 +257,6 @@ def run(*, function: Callable, log_level: str, log_file: str, **kwargs) -> None:
     function(**kwargs)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = cli()
     run(**vars(args))
