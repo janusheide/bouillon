78d79b89c826d221ec70b96b8486d49afecd32f2
Janus Heide <janusheide@gmail.com>
2024-09-28 21:49:00 +0200

    use name-rev git command

--- src/bouillon/git.py
+++ src/bouillon/git.py
@@ -17,8 +17,8 @@ from bouillon import run
 def repository_name() -> str:
     """Get git repository name."""
     r = run(["git", "config", "--get", "remote.origin.url"],
-                     stdout=subprocess.PIPE,
-                     check=True)
+            stdout=subprocess.PIPE,
+            check=True)
 
     return os.path.split(r.stdout.decode().rstrip())[-1].split(".")[0]
 
@@ -26,17 +26,17 @@ def repository_name() -> str:
 def current_branch() -> str:
     """Get git current branch."""
     r = run(["git", "rev-parse", "--abbrev-ref", "HEAD"],
-                     stdout=subprocess.PIPE,
-                     check=True)
+            stdout=subprocess.PIPE,
+            check=True)
 
     return r.stdout.decode().rstrip()
 
 
 def default_branch() -> str:
     """Get git default branch."""
-    r = run(["git", "rev-parse", "--abbrev-ref", "origin/HEAD"],
-                     stdout=subprocess.PIPE,
-                     check=True)
+    r = run(["git" "name-rev" "--name-only" "origin/HEAD"],
+            stdout=subprocess.PIPE,
+            check=True)
 
     return r.stdout.decode().rstrip().lstrip("origin/")
 
@@ -44,8 +44,8 @@ def default_branch() -> str:
 def working_directory_clean() -> bool:
     """Check if the working directory is clean."""
     r = run(["git", "diff", "--quiet", "--exit-code"],
-                     stdout=subprocess.PIPE,
-                     check=True)
+            stdout=subprocess.PIPE,
+            check=True)
 
     return not r.returncode
 
@@ -53,8 +53,8 @@ def working_directory_clean() -> bool:
 def commit_id() -> str:
     """Get current git commit id."""
     r = run(["git", "rev-parse", "HEAD"],
-                     stdout=subprocess.PIPE,
-                     check=True)
+            stdout=subprocess.PIPE,
+            check=True)
 
     return str(r.stdout.decode().rstrip())
 
@@ -62,7 +62,7 @@ def commit_id() -> str:
 def tags() -> list[str]:
     """Get list of all git tags."""
     r = run(["git", "tag"],
-                     stdout=subprocess.PIPE,
-                     check=True)
+            stdout=subprocess.PIPE,
+            check=True)
 
     return r.stdout.decode().rstrip().split("\n")
