ed323bb1c8e42649c73404500ac76b46155e2ad3
Janus Heide <janusheide@gmail.com>
2024-10-07 13:48:30 +0200

    added git.working_directory_updated()

--- test/bouillon/test_git.py
+++ test/bouillon/test_git.py
@@ -3,33 +3,41 @@
 #
 # Distributed under the "BSD 3-Clause License", see LICENSE.txt.
 
-import bouillon
+import pytest
+
+from bouillon import git
 
 
 def test_repository_name():
-    assert bouillon.git.repository_name() == "bouillon"
+    assert git.repository_name() == "bouillon"
 
 
-def test_working_directory_clean(tmpdir):
-    bouillon.run(["git", "stash"])
-    assert bouillon.git.working_directory_clean()
-    bouillon.run(["git", "stash", "apply"])
+@pytest.mark.cicd
+def test_working_directory_clean():
+    """Note will fail if there are unstaged changes in the working dir."""
+    assert git.working_directory_clean()
+
+
+@pytest.mark.cicd
+def test_working_directory_updated():
+    """Note will fail if not up to date with origin."""
+    assert git.working_directory_updated()
 
 
 def test_current_branch():
-    assert isinstance(bouillon.git.current_branch(), str)
+    assert isinstance(git.current_branch(), str)
 
 
 def test_default_branch():
     """28/09/24 Note that this return None on a gitub runner."""
-    assert bouillon.git.default_branch() in ["main", None]
+    assert git.default_branch() in ["main", None]
 
 
 def test_commit_id():
-    assert len(bouillon.git.commit_id()) == 40
+    assert len(git.commit_id()) == 40
 
 
 def test_tags():
-    assert "0.0.1" in bouillon.git.tags()
-    assert "1.0.0" in bouillon.git.tags()
-    assert "1.4.0" in bouillon.git.tags()
+    assert "0.0.1" in git.tags()
+    assert "1.0.0" in git.tags()
+    assert "1.4.0" in git.tags()
